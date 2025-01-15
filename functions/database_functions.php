59335e7e6165f1d55a86fdd3da9b1fcc3c1ae205
bereket-7 <bekhonelign@gmail.com>
2025-01-16 00:17:14 +0300

    insertIntoOrder

--- functions/database_functions.php
+++ functions/database_functions.php
@@ -48,3 +48,13 @@ function getOrderId($conn, $customerid)
 	$row = mysqli_fetch_assoc($result);
 	return $row['orderid'];
 }
+function insertIntoOrder($conn, $customerid, $total_price, $date, $ship_name, $ship_address, $ship_city, $ship_zip_code, $ship_country)
+{
+	$query = "INSERT INTO orders VALUES 
+		('', '" . $customerid . "', '" . $total_price . "', '" . $date . "', '" . $ship_name . "', '" . $ship_address . "', '" . $ship_city . "', '" . $ship_zip_code . "', '" . $ship_country . "')";
+	$result = mysqli_query($conn, $query);
+	if (!$result) {
+		echo "Insert orders failed " . mysqli_error($conn);
+		exit;
+	}
+}
